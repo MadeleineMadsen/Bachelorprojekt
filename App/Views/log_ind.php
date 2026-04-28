9409caa5e5f78ffb84fb847bfea86ad091d50d0c
Madeleine Madsen <mama0002@stud.kea.dk>
2026-04-28 22:05:43 +0200

    Rettelse og opdeling af css

--- App/Views/log_ind.php
+++ App/Views/log_ind.php
@@ -3,9 +3,9 @@
 ?>
 
 <main class="container">
-    <h1>LOG IND</h1>
+    <h1 class="form-title">LOG IND</h1>
 
-    <section class="login-container">
+    <section class="form-container login-container">
         <form method="POST">
             <input type="email" name="email" placeholder="Studiemail" required>
 
@@ -15,85 +15,7 @@
         </form>
 
         <div>
-            <p>Ikke medlem endnu? <a href="opret_dig">Opret dig her</a> </p>
+            <p class="form-link-text">Ikke medlem endnu? <a href="opret_dig">Opret dig her</a> </p>
         </div>
     </section>
 </main>
-
-<style>
-    h1 {
-        font-family: var(--font-header);
-        font-size: var(--font-mobil-h1);
-    }
-
-    .login-container {
-        width: 300px;
-        margin: 30px auto;
-    }
-
-    input {
-        font-family: var(--font-text);
-        font-size: var(--font-mobil-text);
-        width: 100%;
-        border: none;
-        border-bottom: 1px solid var(--font-color-black);
-        padding: var(--gap-sm) 0 var(--gap-sm);
-        margin-bottom: var(--gap-sm);
-        outline: none;
-    }
-
-    input::placeholder {
-        color: var(--font-color-black);
-    }
-
-    p {
-        font-family: var(--font-text);
-        font-size: var(--font-mobil-text);
-        font-weight: var(--poppins-font-bold);
-        margin-top: var(--gap-sm);
-        color: var(--font-color-black);
-
-        a {
-            color: var(--color-light-blue);
-        }
-    }
-
-    @media (min-width: 768px) {
-        h1 {
-            font-family: var(--font-header);
-            font-size: var(--font-desktop-h1);
-        }
-
-        .login-container {
-            width: 720px;
-            margin: 70px auto;
-        }
-
-        input {
-            font-family: var(--font-text);
-            font-size: var(--font-desktop-text-big);
-            color: var(--font-color-black);
-            width: 100%;
-            border: none;
-            border-bottom: 2px solid var(--font-color-black);
-            padding: var(--gap-sm) 0 var(--gap-sm);
-            margin-bottom: var(--gap-md);
-            outline: none;
-        }
-
-        input::placeholder {
-            color: var(--font-color-black);
-        }
-
-        p {
-            font-family: var(--font-text);
-            font-size: var(--font-desktop-text-big);
-            font-weight: var(--poppins-font-bold);
-            margin-top: var(--gap-md);
-
-            a {
-                color: var(--color-light-blue);
-            }
-        }
-    }
-</style>
--- App/Views/medlem_sog.php
+++ App/Views/medlem_sog.php
@@ -4,7 +4,7 @@
 
 <main class="container">
 
-    <section class="membership-container">
+    <section class="form-container membership-container">
         <form method="POST">
             <div class="form-row">
                 <input type="text" name="name" placeholder="Fornavn" required>
@@ -26,101 +26,3 @@
         </form>
     </section>
 </main>
-
-<style>
-    .membership-container {
-        width: 300px;
-        margin: 30px auto;
-    }
-
-    input {
-        font-family: var(--font-text);
-        font-size: var(--font-mobil-text);
-        color: var(--font-color-black);
-        width: 100%;
-        border: none;
-        border-bottom: 1px solid var(--font-color-black);
-        padding: var(--gap-sm) 0 var(--gap-sm);
-        margin-bottom: var(--gap-sm);
-        outline: none;
-    }
-
-    input::placeholder {
-        color: var(--font-color-black);
-    }
-
-    .form-row {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        gap: var(--gap-sm);
-    }
-
-    .form-label {
-        display: block;
-        font-family: var(--font-text);
-        font-size: var(--font-mobil-text);
-        margin-bottom: var(--gap-sm);
-    }
-
-    textarea {
-        width: 100%;
-        height: 170px;
-        resize: none;
-        border: 1px solid var(--font-color-black);
-        margin-bottom: var(--gap-sm);
-        font-family: var(--font-text);
-        font-size: var(--font-mobil-text);
-        padding: var(--gap-sm);
-        outline: none;
-    }
-
-    @media (min-width: 768px) {
-
-        .membership-container {
-            width: 720px;
-            margin: 70px auto;
-        }
-
-        input {
-            font-family: var(--font-text);
-            font-size: var(--font-desktop-text-small);
-            color: var(--font-color-black);
-            width: 100%;
-            border: none;
-            border-bottom: 2px solid var(--font-color-black);
-            padding: var(--gap-sm) 0 var(--gap-sm);
-            margin-bottom: var(--gap-md);
-            outline: none;
-        }
-
-        input::placeholder {
-            color: var(--font-color-black);
-        }
-
-        .form-row {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            gap: var(--gap-md);
-        }
-
-        .form-label {
-            display: block;
-            font-family: var(--font-text);
-            font-size: var(--font-desktop-text-small);
-            margin-bottom: var(--gap-sm);
-        }
-
-        textarea {
-            width: 100%;
-            height: 200px;
-            resize: none;
-            border: 1px solid var(--font-color-black);
-            margin-bottom: var(--gap-md);
-            font-family: var(--font-text);
-            font-size: var(--font-desktop-text-small);
-            padding: var(--gap-sm);
-            outline: none;
-        }
-
-    }
-</style>
