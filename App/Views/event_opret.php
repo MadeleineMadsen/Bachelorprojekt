f3ea5ca804eecf3480c0e53cee16655ce837dc36
Kamilla Huhnke <kamillahuhnke@me.com>
2026-05-19 11:48:09 +0200

    events knap sekundær/primær

--- App/Views/event_opret.php
+++ App/Views/event_opret.php
@@ -70,7 +70,7 @@ $formAction = $isEditing ? '/event_rediger' : '';
                     </p>
                     <img
                         id="uploadPreview"
-                        src="<?= $isEditing && !empty($event['event_image']) ? '/assets/img/' . htmlspecialchars($event['event_image']) : '' ?>"
+                        src="<?= $isEditing && !empty($event['event_image']) ? '' . htmlspecialchars($event['event_image']) : '' ?>"
                         alt="Billede preview"
                         class="upload-preview"
                         <?= (!$isEditing || empty($event['event_image'])) ? 'style="display:none"' : '' ?>
