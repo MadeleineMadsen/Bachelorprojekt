c7082d741f5813af8476f12c889d0ec945361127
Madeleine Madsen <mama0002@stud.kea.dk>
2026-05-21 11:27:45 +0200

    Ny db - Admin kan tilføje studieretning & semester

--- docker/db/bachelor.sql
+++ docker/db/bachelor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Vært: mariadb
--- Genereringstid: 14. 05 2026 kl. 09:32:46
+-- Genereringstid: 21. 05 2026 kl. 09:26:50
 -- Serverversion: 10.6.20-MariaDB-ubu2004
 -- PHP-version: 8.2.27
 
@@ -83,9 +83,9 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`event_pk`, `event_title`, `event_subtitle`, `event_description`, `event_expectations`, `event_date`, `event_time`, `event_end_time`, `event_location`, `category_fk`, `event_image`, `created_by_fk`, `created_at`, `reminder_sent_at`, `deleted_at`) VALUES
-('2327f709-4e02-11f1-975b-0242ac1d0002', 'Karaoke Night', 'Syng dine yndlingssange', 'Tag dine venner med til en sjov karaokeaften med drinks og god stemning.', 'Musik, dans og masser af grin', '2026-06-15', '20:00:00', '23:30:00', 'Downtown Bar', '1', 'fredagsbar-udenfor.webp', 1, '2026-05-12 16:46:58', NULL, NULL),
-('2f7ba6e5-4e02-11f1-975b-0242ac1d0002', 'Coding Bootcamp', 'Lær webudvikling på én dag', 'En intensiv workshop hvor du lærer HTML, CSS og JavaScript fra bunden.', 'Praktiske øvelser og nye skills', '2026-07-01', '09:00:00', '16:00:00', 'KEA Guldbergsgade', '2', 'fredagsbar-udenfor.webp', 1, '2026-05-12 16:47:18', NULL, NULL),
-('912af718-4dff-11f1-975b-0242ac1d0002', 'Sommerfest på taget', 'DJ og drinks hele aftenen', 'Kom og vær med til årets sommerfest med musik, drinks og udsigt over byen.', 'God stemning og fest hele natten', '2026-06-20', '19:00:00', '02:00:00', 'Nørrebro Tagterrasse', '1', 'fredagsbar-udenfor.webp', 1, '2026-05-12 12:39:06', NULL, NULL),
+('2327f709-4e02-11f1-975b-0242ac1d0002', 'Karaoke Night', 'Syng dine yndlingssange', 'Tag dine venner med til en sjov karaokeaften med drinks og god stemning.', 'Musik, dans og masser af grin', '2026-06-15', '20:00:00', '23:30:00', 'Downtown Bar', '1', 'karaoke.webp', 1, '2026-05-12 16:46:58', NULL, NULL),
+('2f7ba6e5-4e02-11f1-975b-0242ac1d0002', 'Coding Bootcamp', 'Lær webudvikling på én dag', 'En intensiv workshop hvor du lærer HTML, CSS og JavaScript fra bunden.', 'Praktiske øvelser og nye skills', '2026-07-01', '09:00:00', '16:00:00', 'KEA Guldbergsgade', '2', 'samling-af-mennesker.webp', 1, '2026-05-12 16:47:18', NULL, NULL),
+('912af718-4dff-11f1-975b-0242ac1d0002', 'Sommerfest på taget', 'DJ og drinks hele aftenen', 'Kom og vær med til årets sommerfest med musik, drinks og udsigt over byen.', 'God stemning og fest hele natten', '2026-06-20', '19:00:00', '02:00:00', 'Nørrebro Tagterrasse', '1', 'sommerfest.webp', 1, '2026-05-12 12:39:06', NULL, NULL),
 ('df5ef636-4478-11f1-b685-0242ac1d0002', 'Fredagsbar', 'GBG Social inviterer til en hyggelig fredagsbar, h.', 'GBG Social inviterer til en hyggelig fredagsbar, hvor studerende kan mødes og skabe nye relationer i afslappede omgivelser.', 'Kolde drinks i baren\r\nGod musik og stemning\r\nMulighed for at møde nye mennesker\r\nHygge og fællesskab', '2026-06-20', '18:00:00', '23:00:00', 'KEA Kantinen', '1', 'fredagsbar-udenfor.webp', 1, '2026-05-07 12:18:16', NULL, NULL),
 ('df5efe72-4478-11f1-b685-0242ac1d0002', 'Fodboldturnering', 'Torsdag d. 5 marts kl. 14:00', 'Kom og vær med til en aktiv dag med fodbold, hvor både begyndere og øvede kan deltage.', 'Holdturnering\r\nPræmier til vinderne\r\nGod energi og fællesskab\r\nMulighed for nye bekendtskaber', '2026-03-05', '14:00:00', '18:00:00', 'KEA Boldbane', '4', 'turnering.webp', 1, '2026-05-07 12:18:16', NULL, NULL),
 ('df5effe9-4478-11f1-b685-0242ac1d0002', 'CV Workshop', 'Tirsdag d. 10 marts kl. 10:00', 'Få hjælp til at optimere dit CV og forbedre dine jobmuligheder gennem sparring og feedback.', 'Feedback på CV\r\nTips til ansøgninger\r\nVejledning fra erfarne studerende\r\nMulighed for spørgsmål', '2026-03-10', '10:00:00', '13:00:00', 'Lokale A1', '2', 'workshop.webp', 1, '2026-05-07 12:18:16', NULL, NULL);
@@ -169,10 +169,24 @@ CREATE TABLE `event_registrations` (
 --
 
 INSERT INTO `event_registrations` (`registration_pk`, `event_fk`, `user_fk`, `registered_at`) VALUES
+('07775ced-23fe-4f5f-a9fd-d8d69fbbd4cb', 'df5efe72-4478-11f1-b685-0242ac1d0002', 17, '2026-05-20 12:14:26'),
+('0b5eee99-a7cc-400c-b9f4-6fbadc128b24', 'df5efe72-4478-11f1-b685-0242ac1d0002', 10, '2026-05-19 12:17:46'),
+('10af5de4-ecf8-4401-ae3a-b95f524a9c69', 'df5efe72-4478-11f1-b685-0242ac1d0002', 16, '2026-05-20 12:14:07'),
+('13474f9a-ffd9-4dd0-b49e-b1936913f337', 'df5efe72-4478-11f1-b685-0242ac1d0002', 22, '2026-05-20 12:17:39'),
+('1e92b2ec-9f28-41e9-ae6f-4ca5ac99ff23', 'df5efe72-4478-11f1-b685-0242ac1d0002', 20, '2026-05-20 12:16:49'),
 ('34e6b8f9-4484-11f1-b685-0242ac1d0002', 'df5ef636-4478-11f1-b685-0242ac1d0002', 1, '2026-05-07 12:18:16'),
 ('34e703af-4484-11f1-b685-0242ac1d0002', 'df5ef636-4478-11f1-b685-0242ac1d0002', 2, '2026-05-07 12:18:16'),
+('45da3b5a-33ac-4f86-9ed6-2b61a9f1918f', 'df5efe72-4478-11f1-b685-0242ac1d0002', 19, '2026-05-20 12:16:23'),
+('49c0c3e3-29cd-46cb-8949-72b453b32398', 'df5efe72-4478-11f1-b685-0242ac1d0002', 13, '2026-05-20 12:13:06'),
+('4ac66ed9-8e68-4928-80a0-4cd5d16181cd', 'df5efe72-4478-11f1-b685-0242ac1d0002', 15, '2026-05-20 12:13:44'),
+('5c9f2e5a-08a2-4400-991d-da2ee5161190', 'df5efe72-4478-11f1-b685-0242ac1d0002', 9, '2026-05-19 12:17:19'),
 ('681d5803-4483-11f1-b685-0242ac1d0002', 'df5ef636-4478-11f1-b685-0242ac1d0002', 4, '2026-05-07 12:18:16'),
-('8472c84e-c6f5-4e60-8e17-cdffb5b2428a', 'df5efe72-4478-11f1-b685-0242ac1d0002', 1, '2026-05-12 12:53:05');
+('8472c84e-c6f5-4e60-8e17-cdffb5b2428a', 'df5efe72-4478-11f1-b685-0242ac1d0002', 1, '2026-05-12 12:53:05'),
+('b54113ea-6a2e-4487-a653-beb3e29d9ad0', 'df5efe72-4478-11f1-b685-0242ac1d0002', 11, '2026-05-19 12:18:08'),
+('b93b718e-fdcc-409f-b7e5-452d8fe83b9a', 'df5efe72-4478-11f1-b685-0242ac1d0002', 14, '2026-05-20 12:13:26'),
+('c743effc-1d94-470c-9096-b4cfab596ca6', 'df5efe72-4478-11f1-b685-0242ac1d0002', 12, '2026-05-19 12:18:41'),
+('de6afda5-853d-4771-9194-aef002336f8a', 'df5efe72-4478-11f1-b685-0242ac1d0002', 18, '2026-05-20 12:14:49'),
+('ea801840-e209-45ca-8c53-22d0fb748b57', 'df5efe72-4478-11f1-b685-0242ac1d0002', 21, '2026-05-20 12:17:10');
 
 -- --------------------------------------------------------
 
@@ -192,6 +206,13 @@ CREATE TABLE `members` (
   `approved_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Data dump for tabellen `members`
+--
+
+INSERT INTO `members` (`member_pk`, `user_fk`, `education_fk`, `semester_fk`, `application_text`, `status`, `approved_by_fk`, `applied_at`, `approved_at`, `deleted_at`) VALUES
+('18109ec1-54f6-11f1-9e03-266b729c1588', 1, 15, 7, NULL, 'approved', 1, '2026-05-21 09:18:56', '2026-05-21 09:18:56', NULL);
 
 --
 -- Triggers/udløsere `members`
@@ -298,9 +319,23 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_pk`, `user_name`, `user_last_name`, `user_email`, `user_password`, `user_created_at`, `user_updated_at`, `user_deleted_at`, `user_verified_at`, `user_verification_key`, `role_fk`, `user_profile_image`, `failed_login_attempts`, `locked_at`, `login_unlock_key`) VALUES
 (1, 'Admin', 'Strator', 'admin@admin.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-12 07:52:23', NULL, '2026-05-12 07:55:15', NULL, '1', NULL, 0, NULL, NULL),
-(2, 'Naomi', 'Rasmussen', 'n@r.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-14 09:32:19', NULL, '2026-05-12 07:52:49', NULL, '3', NULL, 0, NULL, NULL),
-(3, 'Madeleine', 'Madsen', 'm@m.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-12 07:52:18', NULL, '2026-05-12 07:53:12', NULL, '3', NULL, 0, NULL, NULL),
-(4, 'Kamilla', 'Huhnke', 'k@h.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-12 07:52:15', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL);
+(2, 'Naomi', 'Rasmussen', 'n@r.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:20:54', NULL, '2026-05-12 07:52:49', NULL, '3', NULL, 0, NULL, NULL),
+(3, 'Madeleine', 'Madsen', 'm@m.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:24:45', NULL, '2026-05-12 07:53:12', NULL, '3', NULL, 0, NULL, NULL),
+(4, 'Kamilla', 'Huhnke', 'k@h.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:20:59', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(9, 'Test 1', '1', 'test1@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:24:53', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(10, 'Test 2', '2', 'test2@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:24:57', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(11, 'Test 3', '3', 'test3@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-20 18:33:51', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(12, 'Test 4', '4', 'test4@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:25:13', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(13, 'Test 5', '5', 'test5@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:25:11', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(14, 'Test 6', '6', 'test6@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:25:08', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(15, 'Test 7', '7', 'test7@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:25:06', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(16, 'Test 8', '8', 'test8@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-20 18:58:59', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(17, 'Test 9', '9', 'test9@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:25:01', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(18, 'Test 10', '10', 'test10@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-21 09:25:03', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(19, 'Test 11', '11', 'test11@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-19 07:49:47', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(20, 'Test 12', '12', 'test12@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-20 12:16:41', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(21, 'Test 13', '13', 'test13@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-19 07:49:47', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL),
+(22, 'Test 14', '14', 'test14@test.com', '$2y$10$6XVUYnhke5s.EFQi0.FQ.uET2uiYOZysL.ZUsozMxltUIlG5sgs26', '2026-05-07 12:18:16', '2026-05-19 07:49:47', NULL, '2026-05-12 07:54:15', NULL, '3', NULL, 0, NULL, NULL);
 
 --
 -- Begrænsninger for dumpede tabeller
@@ -386,7 +421,7 @@ ALTER TABLE `semesters`
 -- Tilføj AUTO_INCREMENT i tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_pk` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_pk` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Begrænsninger for dumpede tabeller
