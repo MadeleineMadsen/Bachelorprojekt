1e3ab204f5abeaa1a2442aa3c0d68b34d41a7a84
naom0649 <naom0649@stud.kea.dk>
2026-05-22 11:07:11 +0200

    slettet tomme klasser

--- App/Controllers/CalendarController.php
+++
@@ -1,5 +0,0 @@
-<!-- TODO: ret alle kalendre til en enkelt calendar.php og kig efter auth/privilegier her istedet for i hvert enkelt view. -->
-
-<!-- controller til kalender - henter fra eventmodel
-
-- calendar.php -->
